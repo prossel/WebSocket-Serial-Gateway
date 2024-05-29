4795b741e15ee7519e8f37f41f7e4200a33bb58f
Pierre Rossel <>
2024-05-29 02:57:30 +0200

    Add command line and path parameters, /list path command

--- README.md
+++ README.md
@@ -15,7 +15,29 @@ pip3 install websockets pyserial
 
 ## Configuration
 
-Edit gateway.py and configure these values:
+You may edit gateway.py and configure the default values, however, it is recommended to pass the configuration as parameters on the command line or in the path when opening the WebSocket.
+
+### Command line
+
+Serial port, serial baudrate, WebSocket host and port are optional parameters on the command line.
+
+See Usage section below for examples
+
+### WebSocket path
+
+The serial port and baud rate can be given in the path when creating the WebSocket. If provided, the values will override default values and command line parameters.
+
+```js
+socket = new WebSocket("ws://localhost:8765/[device][@baudrate]");
+```
+
+To get the list of available serial ports, use /list in the path. The server will send the list of devices and close the connection.
+
+```js
+socket = new WebSocket("ws://localhost:8765/list");
+```
+
+### Edit gateway.py (not recommended)
 
 ```python
 # Serial port configuration
@@ -29,11 +51,24 @@ ws_port = 8765
 
 ## Usage
 
+### Start the gateway
+
+Basic, will use default value for serial port, baudrate, host and port
+
 ```shell
 python3 gateway.py
 ```
 
-Or for development:
+With parameters to override default values for serial port, baudrate, host and port
+
+```shell
+python3 gateway.py [device] [baudrate] [host] [port]
+python3 gateway.py /dev/cu.usbmodem101 9600 localhost 8765
+```
+
+### Development
+
+Use nodemon to restart the server when the script is modified.
 
 ```shell
 nodemon --exec python3 gateway.py
@@ -45,15 +80,20 @@ Simple HTTP client with p5.js
 
 https://editor.p5js.org/prossel/sketches/fxODyCmxV
 
+WebSocket Serial Monitor with p5.js
+
+https://editor.p5js.org/prossel/sketches/zTU-rAJM6
+
 ## Improvements
 
 The initial version is enough for the project that initiated it. However it would be nice to further improve it to make it more generic.
 
-- Auto detect last serial port
-- Support a list of possible serial port
-- Set the configuration from the command line
-- Set the configuration from a file
-- Serial configuration controlled by the websocket client
+- [ ] Auto detect last serial port
+- [ ] Support a list of possible serial port
+- [x] Set the configuration from the command line
+- [ ] Set the configuration from an external file (gitignored)
+- [x] Serial configuration controlled by the WebSocket client
+- [x] Get the list of available serial ports through WebSocket
 - ...
 
 Don't hesitate to send your pull requests.
--- gateway.py
+++ gateway.py
@@ -5,21 +5,24 @@
 # install modules
 # pip3 install websockets pyserial
 
-# Usage: python3 gateway.py
-# Usage dev: nodemon --exec python3 gateway.py
+# Usage: python3 gateway.py [device] [baudrate] [host] [port]
+# Usage dev: nodemon --exec python3 gateway.py [device] [baudrate] [host] [port]
 
-# Test with https://editor.p5js.org/prossel/sketches/fxODyCmxV
+# Simple test https://editor.p5js.org/prossel/sketches/fxODyCmxV
+# WebSocket Serial Monitor https://editor.p5js.org/prossel/sketches/zTU-rAJM6
 
 # Original repository:
 # https://github.com/prossel/WebSocket-Serial-Gateway
 
 # History:
 # 2024-05-21 - Pierre Rossel - Initial version
+# 2024-05-29 - Pierre Rossel - Added serial port listing, command line parameters and path parameters
 
 
 import asyncio
 import websockets
 import serial
+from serial.tools import list_ports
 
 # Serial port configuration
 ser_port = '/dev/tty.usbmodem101'
@@ -53,6 +56,36 @@ async def serial_task(ser, websocket):
 
 async def websocket_handler(websocket, path):
     
+    print("WebSocket handler started with path: ", path)
+    
+    if path == "/list":
+        # print("Listing serial ports")
+        ports = list_ports.comports()
+        for port in ports:
+            # print(port.device)
+            await websocket.send(port.device)
+        return
+    
+    # path may contain optional serial port and baudrate 
+    # Examples: 
+    # - /
+    # - /@
+    # - /dev/tty.usbmodem101
+    # - /dev/tty.usbmodem101@
+    # - /dev/tty.usbmodem101@115200
+    
+    # extract serial port and baudrate from path
+    global ser_port
+    global ser_baudrate
+    if path != "/":
+        parts = path.split('@')
+        if len(parts[0]) > 1: 
+            ser_port = parts[0]
+        if len(parts) > 1 and len(parts[1]) > 0:
+            ser_baudrate = int(parts[1])
+                
+    print("Waiting for port " + ser_port + " at " + str(ser_baudrate) + " bauds")
+    
     while websocket.open:
 
         # Open the serial port
@@ -60,7 +93,7 @@ async def websocket_handler(websocket, path):
             ser = serial.Serial(ser_port, ser_baudrate)
             print(f'Serial port open on {ser_port} at {ser_baudrate} bauds')
         except serial.SerialException as e:
-            print(f"Serial port open error: {e}")
+            # print(f"Serial port open error: {e}")
             # wait 2 seconds before retrying
             await asyncio.sleep(2)
             continue
@@ -98,6 +131,22 @@ async def websocket_handler(websocket, path):
         ser.close()
      
     print("WebSocket handler ended")
+    
+# get parameters from command line (if any)
+# example: python3 gateway.py /dev/tty.usbmodem101 115200 localhost 8765
+import sys
+if len(sys.argv) > 1:
+    ser_port = sys.argv[1]
+if len(sys.argv) > 2:
+    ser_baudrate = int(sys.argv[2])
+if len(sys.argv) > 3:
+    ws_host = sys.argv[3]
+if len(sys.argv) > 4:
+    ws_port = int(sys.argv[4])
+    
+print(f"Serial port: {ser_port} at {ser_baudrate} bauds")
+print(f"WebSocket server: {ws_host}:{ws_port}")
+
     
 try:
     # Start the WebSocket server
